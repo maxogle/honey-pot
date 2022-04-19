9a906a91b883e697c2b34f7489087caee2841544
alexandermaxwellogle <maxogle@gmail.com>
2022-04-19 17:27:31 -0400

    added json db and routes files

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,29 +2,29 @@ import {React, useState} from 'react'
 
 const Form = ({setData}) => {
     const[form,setForm]=useState({
-        title: '',
-        image: '',
+        name: '',
+        img: '',
         warning_message: ''
     })
 return (
     <div>
-        <form onSubmit={(e)=> {
+        <form onSubmit={async(e)=> {
             e.preventDefault()
-        //     let req = await fetch("https://api.fbi.gov/wanted/v1/list", {
-        //   method: 'POST',
-        //   headers:{'Content-Type': 'application/json'},
-        //   body: JSON.stringify(form)
-        // })
-        // let res = await req.json()
-        // setData(prevState => [...prevState, res] )
+            let req = await fetch("http://localhost:8000/posts/posts/1", {
+          method: 'POST',
+          headers:{'Content-Type': 'application/json'},
+          body: JSON.stringify(form)
+        })
+        let res = await req.json()
+        setData(prevState => [...prevState, res] )
 
         }}>
     <div>
-        <label>title</label>
+        <label>Name</label>
         <input type="text" name="title" onChange={(e)=>{setForm({...form, title: e.target.value})}}/>
-        <label>image</label>
+        <label>"Profile" Photo</label>
         <input type="text" name="img" onChange={(e)=>{setForm({...form, img: e.target.value})}}/>
-        <label>warning_message</label>
+        <label>What you'd be wanted for ;)</label>
         <input type="text" name="warning_message" onChange={(e)=>{setForm({...form, warning_message: e.target.value})}}/>
     </div>
     <button type="submit">
